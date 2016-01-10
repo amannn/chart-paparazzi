5159d3f902a0fc29a8eea4f6b6155e3cba453c42
Jan Amann <jan@amann.me>
2016-01-10 20:40:52 +0100

    cleaned up some whitespace

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ jsonFile.open(URL_JSON, function () {
   var chartsData = JSON.parse(jsonFile.plainText);
   var i = 0;
 
-  // Recursively calls itslef after screenshot is made  
+  // Recursively calls itslef after screenshot is made
   function screenshotChart(num) {
 
     // Open webpage like localhost:8080?23
     renderPage.open(URL_RENDER + '?' + i, function(status) {
 
-      if(status !== 'success') {
+      if (status !== 'success') {
         console.log('Error loading chart at pos ' + i)
       } else {
         var fileName = FILE_PREFIX
@@ -33,9 +33,9 @@ jsonFile.open(URL_JSON, function () {
         console.log('capturing ' + fileName);
         renderPage.render(PATH_SCREENSHOTS + fileName);
       }
-      
+
       // Go for the next screenshot if there is another one or exit
-      if(i !== chartsData.length - 1)
+      if (i !== chartsData.length - 1)
         screenshotChart(++i);
       else {
         console.log('\n** Captured ' + (i+1) + ' screenshots **\n');
